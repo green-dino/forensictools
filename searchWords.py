a931608ada322e3bba23592c7a81b70b42020dc0
green-dino <104174275+green-dino@users.noreply.github.com>
2023-09-25 10:31:43 -0700

    custom path, loop

--- searchWords.py
+++ searchWords.py
@@ -1,24 +1,33 @@
 import sys
 
-searchWords = set()
+def load_search_words(file_path):
+    search_words = set()
+    try:
+        with open(file_path) as fileWords:
+            for line in fileWords:
+                search_words.add(line.strip())
+    except FileNotFoundError:
+        print(f'The file {file_path} does not exist. Please create the file or add words to it.')
+    except Exception as e:
+        print('File Handling Error:', str(e))
+    return search_words
 
-try:
+while True:
     file_path = input('Enter the file path and name you want to search through: ')
-    with open(file_path) as fileWords:
-        for line in fileWords:
-            searchWords.add(line.strip())
-except FileNotFoundError:
-    print(f'The file {file_path} does not exist. Please create the file or add words to it.')
-    sys.exit()
-except Exception as e:
-    print('File Handling Error:', str(e))
-    sys.exit()
+    searchWords = load_search_words(file_path)
 
-print(searchWords)
+    if not searchWords:
+        continue
 
-word_to_search = input('Enter a word to search: ')
+    print(searchWords)
 
-if word_to_search in searchWords:
-    print('Found Word')
-else:
-    print('Not found')
+    word_to_search = input('Enter a word to search: ')
+
+    if word_to_search.lower() in (word.lower() for word in searchWords):
+        print('Found Word')
+    else:
+        print('Not found')
+
+    again = input('Do you want to search again? (yes/no): ')
+    if again.lower() != 'yes':
+        break
