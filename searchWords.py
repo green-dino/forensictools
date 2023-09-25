826a5aa54d559792be74bd5f6a2c63b2be562f8b
green-dino <104174275+green-dino@users.noreply.github.com>
2023-09-25 10:34:35 -0700

    word frequency analysis

--- searchWords.py
+++ searchWords.py
@@ -1,4 +1,5 @@
 import sys
+import string
 
 def load_search_words(file_path):
     search_words = set()
@@ -11,6 +12,17 @@ def load_search_words(file_path):
     except Exception as e:
         print('File Handling Error:', str(e))
     return search_words
+
+def word_frequency_analysis(search_words):
+    word_count = {}
+    for word in search_words:
+        # Remove punctuation and convert to lowercase for consistency
+        cleaned_word = word.translate(str.maketrans('', '', string.punctuation)).lower()
+        if cleaned_word in word_count:
+            word_count[cleaned_word] += 1
+        else:
+            word_count[cleaned_word] = 1
+    return word_count
 
 while True:
     file_path = input('Enter the file path and name you want to search through: ')
@@ -27,6 +39,12 @@ while True:
         print('Found Word')
     else:
         print('Not found')
+
+    # Perform Word Frequency Analysis
+    word_count = word_frequency_analysis(searchWords)
+    print('Word Frequency Analysis:')
+    for word, count in word_count.items():
+        print(f'{word}: {count}')
 
     again = input('Do you want to search again? (yes/no): ')
     if again.lower() != 'yes':
