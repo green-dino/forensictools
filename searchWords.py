5a5b96f6ca23d8ba72046b23549cbc28b9963dd4
green-dino <104174275+green-dino@users.noreply.github.com>
2023-09-25 10:25:46 -0700

    added custom path and word

--- searchWords.py
+++ searchWords.py
@@ -1,15 +1,24 @@
 import sys
+
 searchWords = set()
+
 try:
-    fileWords = open('bike.txt')
-    for line in fileWords:
-        searchWords.add(line.strip())
-except:
-    print('File Handling Error')
+    file_path = input('Enter the file path and name you want to search through: ')
+    with open(file_path) as fileWords:
+        for line in fileWords:
+            searchWords.add(line.strip())
+except FileNotFoundError:
+    print(f'The file {file_path} does not exist. Please create the file or add words to it.')
     sys.exit()
+except Exception as e:
+    print('File Handling Error:', str(e))
+    sys.exit()
+
 print(searchWords)
-if ('Touring' in searchWords):
+
+word_to_search = input('Enter a word to search: ')
+
+if word_to_search in searchWords:
     print('Found Word')
 else:
-    print('not found')
-    
+    print('Not found')
