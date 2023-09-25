098f3126f345381a3bee15928136f1ce298da23b
green-dino <104174275+green-dino@users.noreply.github.com>
2023-09-25 11:52:46 -0700

    folderwalk

---
+++ folder.py
@@ -0,0 +1,50 @@
+# Python Standard Libraries
+import os
+import sys  # System Methods
+import hashlib
+import argparse
+from datetime import datetime
+
+# Python 3rd Party Libraries
+from prettytable import PrettyTable  # pip install prettytable
+
+# Psuedo Constants
+
+targetFolder = input("Enter Target Folder: ")
+
+# Start of the Script
+print("Walking: ", targetFolder, "\n")
+
+tbl = PrettyTable(['FilePath', 'FileSize', 'Modified Time', 'Access Time', 'Created Time', 'SHA-256 Hash'])
+
+for currentRoot, dirList, fileList in os.walk(targetFolder):
+
+    for nextFile in fileList:
+
+        fullPath = os.path.join(currentRoot, nextFile)
+        absPath = os.path.abspath(fullPath)
+        fileSize = os.path.getsize(absPath)
+
+        # MAC Times (in human-readable form)
+        modified_time = os.path.getmtime(absPath)
+        accessed_time = os.path.getatime(absPath)
+        created_time = os.path.getctime(absPath)
+
+        modified_time_str = datetime.fromtimestamp(modified_time).strftime('%Y-%m-%d %H:%M:%S')
+        accessed_time_str = datetime.fromtimestamp(accessed_time).strftime('%Y-%m-%d %H:%M:%S')
+        created_time_str = datetime.fromtimestamp(created_time).strftime('%Y-%m-%d %H:%M:%S')
+
+        # SHA-256 Hash value of the content of each file
+        sha256_hash = hashlib.sha256()
+        with open(absPath, "rb") as file:
+            while chunk := file.read(8192):
+                sha256_hash.update(chunk)
+        sha256_hash_value = sha256_hash.hexdigest()
+
+        tbl.add_row([absPath, fileSize, modified_time_str, accessed_time_str, created_time_str, sha256_hash_value])
+
+tbl.align = "l"  # align the columns left justified
+# display the table
+print(tbl.get_string(sortby="FileSize", reversesort=True))
+
+print("\nScript-End\n")
