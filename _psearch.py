f56273b8cdcd784aba5aa5ff3d22eeecf24c568d
green-dino <104174275+green-dino@users.noreply.github.com>
2023-09-25 11:02:40 -0700

    updated argparse

--- _psearch.py
+++ _psearch.py
@@ -1,33 +1,51 @@
-import sys
+# psearch support functions 
+
+import argparse
 import os
 import logging
 
+log = logging.getLogger('main._psearch')
 
-def ParseCommandLine():
+#constants
+MIN_WORD = 5 # min word size in bytes
+MAX_WORD = 15 # max word size in bytes
+PREDECESSOR_SIZE = 32 # values to print before match found
+WINDOW_SIZE = 128  # values to dump when match 
+
+# parse command function, uses argparse 
+
+def ParseCommanLine():
     parser = argparse.ArgumentParser('Python Search')
+    parser.add_argument('-v', '--verbose', help='enables the printing of additional program messages', action='store_true')
+    parser.add_argument('-k', '--keyWords', type=ValidateFileRead,
+                        required=True,help='Specify the file containing search words')
+    parser.add_argument('-t','--srchTarget', type=ValidateFileRead,
+                        required=True, help="specifiy the target file to search")
+    parser.add_argument('-m','--theMatrix', type=ValidateFileRead,
+                        required=True, help='Specify the weight matrix file')
+    
+    global gl_args
+    gl_args = parser.parse_args
 
-    parser.add_argument('-v', '--verbose', help="Enables Printing of Program messages", action='store_true')
+    DisplayMessage('Command line processed: Successfully')
 
-    parser.add_argument('-k','--keyWords', type=ValidateFileRead, required=True, help='Specifify the file containing search words')
+    return
 
-    parser.add_argument('-t','--srchTarget', type=ValidateFileRead, required=True, help='Specify the target file to search')
 
-global gl_args
-gl_args = parser.parse_args()
-DisplayMessage("Command Line processed: Successfully")
-return
+#validate read function
 
 def ValidateFileRead(theFile):
-    # Check validation of path
+    #os path valid
     if not os.path.exists(theFile):
-        raise argparse.ArgumentTypeError('File Does not exist')
-    #validate path readable
+        raise argparse.ArgumentTypeError('File does not exist')
+    
+    #readable path
     if os.access(theFile, os.R_OK):
         return theFile
-    else
+    else:
         raise argparse.ArgumentTypeError('File is not readable')
 
-def SearchWords():
-    searchWords=set()
-    try:
-        fileWords = open(gl_args.keyWords)
+def DisplayMessage(msg):
+    if gl_args.verbose:
+        print(msg)
+    return
